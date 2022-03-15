0faf868ac0edd8d9ac13fd8bf77c5262a0fb7679
naser <n.abkar9@gmail.com>
2022-03-15 21:47:02 +0300

    complate setting page

--- lib/core/widgets/switch_language.dart
+++ lib/core/widgets/switch_language.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:holping_needy_project/localization/loale_lang/app_long.dart';
+import 'package:holping_needy_project/localization/t_key_v.dart';
 
 class SwitchLanguageApp extends StatelessWidget {
   const SwitchLanguageApp({Key? key}) : super(key: key);
@@ -13,14 +14,15 @@ class SwitchLanguageApp extends StatelessWidget {
     String localeAr = 'ar';
 
 
+
     return GetBuilder<AppLang>(
         init: AppLang(),
         builder: (c) {
           return DropdownButton(
               items: [
-                DropdownMenuItem(child: Text(localeEn), value:localeEn),
+                DropdownMenuItem(child: Text(TKeys().localeEnText), value:localeEn),
                 DropdownMenuItem(
-                  child: Text(localeAr),
+                  child: Text(TKeys().localeArText),
                   value: localeAr,
                 )
               ],
